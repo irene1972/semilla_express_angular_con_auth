c5e721fc4217798ac9af7f650ad27b455dc47605
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-27 19:48:33 +0100

    implementar el método insert en el modelo de usuario

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,25 @@
 import pool from '../config/db.js';
 //import mysql from 'mysql2/promise';
 
-export class User{
-    constructor(role,name,surname,nick,email,password,image){
-        this.id=0;
-        this.role=role;
-        this.name=name;
-        this.surname=surname;
-        this.nick=nick;
-        this.email=email;
-        this.password=password;
-        this.image=image;
-        this.created_at=new Date();
-        this.updated_at=new Date();
+export class User {
+    constructor(name, email, password, remember_token, surname = null, nick = null, image = null, role = 'user') {
+        this.id = 0;
+        this.role = role;
+        this.name = name;
+        this.surname = surname;
+        this.nick = nick;
+        this.email = email;
+        this.password = password;
+        this.image = image;
+        this.confirmado = null;
+        this.remember_token = remember_token;
+        this.created_at = new Date();
+        this.updated_at = new Date();
     }
 
-    async getAll(){
+    async getAll() {
         try {
-            const result=await pool.query('SELECT * FROM users');
+            const result = await pool.query('SELECT * FROM users');
             return result;
         } catch (error) {
             return false;
@@ -34,6 +36,17 @@ export class User{
         const result = await pool.query(sql, values);
         return result;
         */
-        
+
+    }
+
+    async insert() {
+        try {
+            const result = await pool.query('INSERT INTO users (role,name,email,password,remember_token,surname,nick,created_at,updated_at) VALUES (?,?,?,?,?,?,?,?,?)', [
+                this.role, this.name, this.email, this.password, this.remember_token, this.surname, this.nick, this.created_at, this.updated_at
+            ]);
+            return result;
+        } catch (error) {
+            return false;
+        }
     }
 }
